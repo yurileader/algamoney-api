9a95d514f57a0f3788d8bdcd49f56cde306b91fd
Yuri <yurileader@gmail.com>
2020-04-14 14:49:06 -0300

    Criando Categoria /modelo /recurso

---
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.example.algamoneyapi.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.algamoneyapi.model.Categoria;
+import com.example.algamoneyapi.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public List<Categoria> listar(){
+		
+		return categoriaRepository.findAll();
+		
+	}
+}

1762f040310e2a34d7bb508cf059764e66dd8cff
Yuri <yurileader@gmail.com>
2020-04-18 16:31:32 -0300

    Atualizando pessoa com PUT

--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -5,14 +5,17 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -67,4 +70,20 @@ public class PessoaResource {
 		pessoaRepository.deleteById(codigo);
 	}
 	
+	
+	
+	@PutMapping("/{codigo}")
+	public Pessoa atualizarPessoa(@PathVariable Long codigo,@Valid @RequestBody Pessoa pessoa ){
+		Pessoa pessoaSalva = this.pessoaRepository.findById(codigo)
+			      .orElseThrow(() -> new EmptyResultDataAccessException(1));
+		/**BeansUtils pode ser usado para ajudar a tratar os dados para atualziar
+		 * Source: A fonte dos dados - no caso da classe pessoas
+		 * target: Para onde irei mandar os dados - no caso para minha variavel pessoaSalva
+		 * ignoreProperties: qual dado devo ignorar - no caso o codigo que é codigo*/
+		//BeanUtils.copyProperties(source, target, ignoreProperties);
+
+			  BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+
+			  return this.pessoaRepository.save(pessoaSalva);
+}
 }

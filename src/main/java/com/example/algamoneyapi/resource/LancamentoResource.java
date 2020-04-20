3f66d0d6fed615e5136341e19832f465ff66f975
Yuri <yurileader@gmail.com>
2020-04-20 13:00:37 -0300

    INICIO Desafio: Lista e busca de lançamentos

---
+++ src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
@@ -0,0 +1,26 @@
+package com.example.algamoneyapi.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.algamoneyapi.model.Lancamento;
+import com.example.algamoneyapi.repository.LancamentoRepository;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+	
+	
+	@GetMapping
+	public List<Lancamento> listar(){
+		return lancamentoRepository.findAll();
+	}
+	
+}

7b9e2ded127a7ff7e59cc48a887c7a095bcad27f
Yuri <yurileader@gmail.com>
2020-04-20 13:16:32 -0300

    implementação de Listar todos e listar pod id

--- src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoneyapi/resource/LancamentoResource.java
@@ -3,7 +3,9 @@ package com.example.algamoneyapi.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +25,12 @@ public class LancamentoResource {
 		return lancamentoRepository.findAll();
 	}
 	
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
+		
+	  return this.lancamentoRepository.findById(codigo)
+	      .map(lancamento -> ResponseEntity.ok(lancamento))
+	      .orElse(ResponseEntity.notFound().build());
+	}
 }

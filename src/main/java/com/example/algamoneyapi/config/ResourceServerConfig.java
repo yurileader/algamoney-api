f033edb803c4c77a53f0eb9adf0819b8ba95c3fa
Yuri <yurileader@gmail.com>
2020-04-26 07:34:30 -0300

    Implementando segurança com OAuth 2 e Password Flow

--- src/main/java/com/example/algamoneyapi/config/ResourceServerConfig.java
+++ src/main/java/com/example/algamoneyapi/config/ResourceServerConfig.java
@@ -1,38 +1,45 @@
 package com.example.algamoneyapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration  // A anotação @EnableWebSecurity já possui a @Configuration. Mas coloco para lembrar que é uma classe de configuração
+@EnableResourceServer // Incluir para implementar o oauth2
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
-	/**Sobrescrever o metodo configure(AuthenticationManagerBuilder*/
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 
 		auth.inMemoryAuthentication()
-		.withUser("admin").password("{noop}admin").roles("ROLE");
+		.withUser("admin").password("admin").roles("ROLE");
 	}
 
 
-	/**Sobrescrver o metodo configure(HttpSecurity http)*/
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
 			.antMatchers("/categorias").permitAll()
 			.anyRequest().authenticated()
-		.and().httpBasic()
 		.and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //Sem manter sessão
 		.and()
 		.csrf().disable();
 	}
 
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		
+		resources.stateless(true);
+	}
 }

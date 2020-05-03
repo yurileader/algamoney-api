dad6203762dec12439f4f21b9ad739b6941cc609
Yuri <yurileader@gmail.com>
2020-05-03 13:08:56 -0300

    Adicionando permissões de acesso

--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,6 +40,7 @@ public class PessoaResource {
 	
 	
 	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
 	public List<Pessoa> listarPessoa(){
 		
 		return pessoaRepository.findAll();
@@ -46,6 +48,7 @@ public class PessoaResource {
 	
 	
 	@GetMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
 		
 	  return this.pessoaRepository.findById(codigo)
@@ -56,6 +59,7 @@ public class PessoaResource {
 
 	
 	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
 	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 		
@@ -68,6 +72,7 @@ public class PessoaResource {
 	
 	@DeleteMapping("/{codigo}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasAuthority('ROLE_REMOVER_PESSOA')")
 	public void remover(@PathVariable Long codigo) {
 
 		pessoaRepository.deleteById(codigo);
@@ -76,6 +81,7 @@ public class PessoaResource {
 	
 	
 	@PutMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA')")
 	public Pessoa atualizarPessoa(@PathVariable Long codigo,@Valid @RequestBody Pessoa pessoa ){
 
 			  Pessoa pessoaSalva = pessoaService.atualizar(pessoa, codigo);
@@ -85,6 +91,7 @@ public class PessoaResource {
 	
 	@PutMapping("/{codigo}/ativo")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA')")
 	public void atualizarPropriedadeAtivo (@PathVariable Long codigo,@RequestBody Boolean ativo) {
 		
 		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
